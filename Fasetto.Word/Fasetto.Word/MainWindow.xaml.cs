da295b9042bd5f27f64e69b5e5eeef926870a26d
spolknut <merkel.johnny@gmail.com>
2021-05-09 20:25:15 +0200

    Added additional method to view model base class

---
+++ Fasetto.Word/Fasetto.Word/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls.Ribbon;
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
